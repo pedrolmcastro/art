a74b6d755f3f173b2a8f63f8131060012cf267cb
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2024-05-07 22:50:24 -0300

    feat: add satisfied_by_all_of, satisfied_by_any_of and satisfied_by_none_of

--- include/art/core/satisfies.hpp
+++ include/art/core/satisfies.hpp
@@ -7,7 +7,6 @@ namespace art {
     template<class T, auto Concept>
     concept satisfies = requires() { Concept.template operator()<T>(); };
 
-
     template<class T, template<class> class Transform, auto Concept>
     concept satisfies_after = satisfies<Transform<T>, Concept>;
 
@@ -20,6 +19,16 @@ namespace art {
 
     template<class T, auto... Concepts>
     concept satisfies_none_of = not satisfies_any_of<T, Concepts...>;
+
+
+    template<auto Concept, class... Types>
+    concept satisfied_by_all_of = (... and satisfies<Types, Concept>);
+
+    template<auto Concept, class... Types>
+    concept satisfied_by_any_of = (... or satisfies<Types, Concept>);
+
+    template<auto Concept, class... Types>
+    concept satisfied_by_none_of = not satisfied_by_any_of<Concept, Types...>;
 }
 
 
--- tests/src/core/satisfies.cpp
+++ tests/src/core/satisfies.cpp
@@ -4,9 +4,8 @@
 #include "art/core/satisfies.hpp"
 
 
-using std::make_signed_t, std::signed_integral, std::unsigned_integral;
-
-using art::satisfies, art::satisfies_after, art::satisfies_all_of, art::satisfies_any_of, art::satisfies_none_of;
+using namespace std;
+using namespace art;
 
 
 static_assert(not satisfies<int, 0>); // Not the intended usage
@@ -17,6 +16,7 @@ static_assert(    satisfies_after<int, make_signed_t, []<signed_integral>{}>);
 static_assert(not satisfies_after<int, make_signed_t, []<unsigned_integral>{}>);
 static_assert(    satisfies_after<unsigned, make_signed_t, []<signed_integral>{}>);
 static_assert(not satisfies_after<unsigned, make_signed_t, []<unsigned_integral>{}>);
+
 
 static_assert(    satisfies_all_of<int>);
 static_assert(    satisfies_all_of<int, []<signed_integral>{}>);
@@ -32,6 +32,22 @@ static_assert(    satisfies_none_of<int>);
 static_assert(not satisfies_none_of<int, []<signed_integral>{}>);
 static_assert(    satisfies_none_of<int, []<unsigned_integral>{}>);
 static_assert(not satisfies_none_of<int, []<signed_integral>{}, []<unsigned_integral>{}>);
+
+
+static_assert(    satisfied_by_all_of<[]<signed_integral>{}>);
+static_assert(    satisfied_by_all_of<[]<signed_integral>{}, int>);
+static_assert(not satisfied_by_all_of<[]<signed_integral>{}, unsigned>);
+static_assert(not satisfied_by_all_of<[]<signed_integral>{}, int, unsigned>);
+
+static_assert(not satisfied_by_any_of<[]<signed_integral>{}>);
+static_assert(    satisfied_by_any_of<[]<signed_integral>{}, int>);
+static_assert(not satisfied_by_any_of<[]<signed_integral>{}, unsigned>);
+static_assert(    satisfied_by_any_of<[]<signed_integral>{}, int, unsigned>);
+
+static_assert(    satisfied_by_none_of<[]<signed_integral>{}>);
+static_assert(not satisfied_by_none_of<[]<signed_integral>{}, int>);
+static_assert(    satisfied_by_none_of<[]<signed_integral>{}, unsigned>);
+static_assert(not satisfied_by_none_of<[]<signed_integral>{}, int, unsigned>);
 
 
 int main() {}
